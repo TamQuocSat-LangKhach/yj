5e2255a60bc9d308ae839d74124129971160caf3
Ho-spair <linyuy@163.com>
2023-12-10 23:50:26 +0800

    修复技能”矢北“

--- yj2014.lua
+++ yj2014.lua
@@ -979,7 +979,7 @@ local shibei = fk.CreateTriggerSkill{
   end,
   on_use = function(self, event, target, player, data)
     local room = player.room
-    if player:getMark("shibei-turn") == 0 then
+    if U.getActualDamageEvents(room, 1, function(e) return e.data[1].to == player end)[1].data[1] == data then
       player:broadcastSkillInvoke(self.name, 1)
       room:notifySkillInvoked(player, self.name)
       room:recover{
@@ -992,7 +992,6 @@ local shibei = fk.CreateTriggerSkill{
       room:notifySkillInvoked(player, self.name, "negative")
       room:loseHp(player, 1, self.name)
     end
-    room:addPlayerMark(player, "shibei-turn", 1)
   end,
 }
 jvshou:addSkill(jianying)
